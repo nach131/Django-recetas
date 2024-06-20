a27ac863c5fbd2676df8f48f30b60182449e3c33
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2024-06-20 18:56:22 +0200

    upgrade

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 		'ENGINE': 'django.db.backends.postgresql',
-		'HOST': os.enviro.get('DB_HOST'),
-		'NAME': os.enviro.get('DB_NAME'),
-		'USER': os.enviro.get('DB_USER'),
-		'PASSWORD': os.enviro.get('DB_PASS'),
+		'HOST': os.environ.get('DB_HOST'),
+		'NAME': os.environ.get('DB_NAME'),
+		'USER': os.environ.get('DB_USER'),
+		'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
