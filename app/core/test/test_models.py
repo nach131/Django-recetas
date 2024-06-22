d91f1dd53f5f09c9447dc7a4bde885a8e5a58f65
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2024-06-22 20:06:24 +0200

    flake8 OK

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -6,16 +6,16 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTest(TestCase):
-  """Test models."""
-  
-  def test_create_user_with_email_successful(self):
-    """Test create email with email is successfull"""
-    email = 'test@enunpimpam.com'
-    password= '12345'
-    user = get_user_model().objects.create_user(
-      email=email,
-      password=password,
-    )
+    """Test models."""
 
-    self.assertEqual(user.email, email)
-    self.assertTrue(user.check_password(password))
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful"""
+        email = 'test@enunpimpam.com'
+        password = '12345'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
